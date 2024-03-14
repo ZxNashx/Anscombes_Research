52ba5ea5a0319ecfca299f4826823e5179146a93
Nash <axylschulz0@gmail.com>
2024-03-14 11:53:06 -0600

    Starting Code Clean

--- stageTests.py
+++ stageTests.py
@@ -8,12 +8,13 @@ ybar = 7.5
 n = 11.0
 variance = 33/8
 slope = 1/2
+intercept = 3
 
 ##
 ##
 # stage 1
 
-x = np.arange(4, 15)  # x values from 4 to 15
+x = np.arange(4, n+4)  # x values from 4 to 15
 M = np.column_stack((x**2, x, np.ones(len(x))))
 
 a, b, c = sp.symbols('a b c')
@@ -29,17 +30,17 @@ print(result)
 
 # stage 2.1
 ones_vector = sp.ones(result.shape[0], 1)
-equation1 = sp.Eq(result.dot(ones_vector), 11 * ybar)
+equation1 = sp.Eq(result.dot(ones_vector), n * ybar)
 solution1 = sp.solve(equation1, (a, b, c))
 print("\n\nTHE RESULTING VECTOR 2.1")
 print(equation1)
 
 #stage 2.2
-modified_vector = result - 7.5 * ones_vector
+modified_vector = result - ybar * ones_vector
 
 modified_dot_product = modified_vector.dot(modified_vector)
 
-equation2 = sp.Eq(modified_dot_product, 10 * (33/8))
+equation2 = sp.Eq(modified_dot_product, (n-1) * variance)
 
 solution2 = sp.solve(equation2, (a, b, c))
 print("\n\nTHE RESULTING VECTOR 2.2")
@@ -49,8 +50,8 @@ print(equation2)
 
 x_vector = sp.Matrix([4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
 modified_x_vector = x_vector - 9 * ones_vector
-modified_result_vector = result - 7.5 * ones_vector
-equation3 = sp.Eq(modified_x_vector.dot(modified_result_vector), 10 * 11 * 1/2)
+modified_result_vector = result - ybar * ones_vector
+equation3 = sp.Eq(modified_x_vector.dot(modified_result_vector), (n-1) * n * slope)
 
 # Create a vector of ones
 ones_vector = sp.ones(len(x_vector), 1)
@@ -72,7 +73,7 @@ for s in solution_final:
     print(get_q(s))
 
 def line_equation(x):
-    return 1/2 * x + 3
+    return slope * x + intercept
 
 def parabola_equation(x, a, b, c):
     return a * x**2 + b * x + c
@@ -82,12 +83,12 @@ def add_parabola_to_plot(a, b, c, x_values, label):
     plt.plot(x_values, y_values, label=label)
 
 x_line_values = np.linspace(0, 20, 400)  # x values for the line
-x_parabola_values = np.linspace(4, 15, 400)  # x values for the parabola
+x_parabola_values = np.linspace(4, n+4, 400)  # x values for the parabola
 
 plt.figure(figsize=(6, 4))
 
 y_line_values = line_equation(x_line_values)
-plt.plot(x_line_values, y_line_values, label='y = 1/2x + 3')
+plt.plot(x_line_values, y_line_values, label='y = b1x + b0')
 
 for s in solution_final:
     a, b, c = s
